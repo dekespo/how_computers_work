93b5d1bc3d9139bb7fa9ec11dfdd15b4de532dbe
dekespo <dekespo@gmail.com>
2015-09-15 16:29:19 +0200

    basic gates and tests finished

--- gates.py
+++ gates.py
@@ -33,52 +33,86 @@ def XOR(a, b):
 # Multiplexor: MUX
 def MUX(a, b, sel):
     checkBit(a); checkBit(b); checkBit(sel)
-    if sel == 0: return a
-    else: return b
+    return OR(AND(a,NOT(sel)), AND(b,sel))
 
 # Demultiplexor: DMUX
-def DMUX(bit, sel): # Bug not made with gates!!
-    if sel == 0: return bit, 0
-    else: return 0, bit
+def DMUX(bit, sel): 
+    return AND(NOT(sel),bit), AND(sel,bit)
 
 # n-bit version of Basic Gates
+# For the sake simplicty n-bits will be:
+# 4 (2**2)
+# 8 (2**3)
+# 16 (2**4)
 
 # Check if the sizes of gates are the same
-def sizeCheck(g1, g2, nbits):
-    if nbits == len(g1) and len(g1) == len(g2): pass
+def sizeCheckn(g1, g2, n):
+    if len(g1) == n and len(g1) == len(g2): pass
     else: sys.exit("THE INPUTS MUST THE SAME NUMBER OF BITS, ABORTING THE MISSION!")
 
-#Gate: n-NOT
-def NOTn(a, n):
-    sizeCheck(a, a, n) # For this case
-    return [NOT(a[i]) for i in xrange(n)]
+#Gate: 16-NOT
+def NOT16(a):
+    n = 16
+    sizeCheckn(a, a, n) 
+    return [NOT(a[i]) for i in range(n)]
 
-#Gate: n-AND
-def ANDn(a, b, n):
-    sizeCheck(a, b, n) 
-    return [AND(a[i], b[i]) for i in xrange(n)]
+#Gate: 16-AND
+def AND16(a, b):
+    n = 16
+    sizeCheckn(a, b, n) 
+    return [AND(a[i], b[i]) for i in range(n)]
 
-#Gate: n-OR
-def ORn(a, b, n):
-    sizeCheck(a, b, n) 
-    return [OR(a[i], b[i]) for i in xrange(n)]
+#Gate: 16-OR
+def OR16(a, b):
+    n = 16
+    sizeCheckn(a, b, n) 
+    return [OR(a[i], b[i]) for i in range(n)]
 
-#n-Multiplexor: MUXn
-def MUXn(a, b, n, sel):
-    sizeCheck(a, b, n) 
-    for i in xrange(n): checkBit(a[i]); checkBit(b[i])
+#16-Multiplexor: MUX16
+def MUX16(a, b, sel):
+    n = 16
+    sizeCheckn(a, b, n) 
+    for i in range(n): 
+        checkBit(a[i]); checkBit(b[i])
     checkBit(sel)
-    if sel == 0: return a
-    else: return b
+    sel16 = [sel] * n # 16 different wires?
+    return OR16(AND16(a,NOT16(sel16)), AND16(b,sel16))
 
-#m-way OR gate: ORmWays
-def ORnWays(a, m):
-    sizeCheck(a, a, m) # For this case
-    part = OR(a[0],a[1])
-    for i in xrange(2,m):
-        part = OR(a[i],part)
-    return part
+#8-way OR gate: OR8Ways
+def OR_8Ways(a): # It can be solved by recursion
+    n = 8
+    sizeCheckn(a, a, n) 
+    a1 = OR(a[0], a[1])
+    a2 = OR(a[2], a[3])
+    a3 = OR(a[4], a[5])
+    a4 = OR(a[6], a[7])
+    b1 = OR(a1, a2)
+    b2 = OR(a3, a4)
+    return OR(b1, b2)
 
-#m-way n-bit Multiplexor: MUXnmWays
-#def MUXnmWays()
+#4-way 16-bit Multiplexor: MUX16_4Ways
+def MUX16_4Ways(a, b, c, d, sel2):
+    n = 16
+    sizeCheckn(a, b, n) 
+    sizeCheckn(c, d, n) 
+    for i in range(n): 
+        checkBit(a[i]); checkBit(b[i])
+        checkBit(c[i]); checkBit(d[i])
+    for i in range(2):
+        checkBit(sel2[i])
+    sel0_16 = [sel2[0]] * n # 16 different wires?
+    sel1_16 = [sel2[1]] * n # 16 different wires?
 
+    return OR16(
+       AND16(NOT16(sel0_16),MUX16(a, b, sel2[1])),
+       AND16(sel0_16,MUX16(c, d, sel2[1]))
+       )
+
+#4-way Demultiplexor: DEMUX_4Ways 
+def DEMUX_4Ways(bit, sel2):
+    checkBit(bit);
+    for i in range(2): checkBit(sel2[i])
+    return AND(AND(NOT(sel2[0]), NOT(sel2[1])),bit),\
+           AND(AND(NOT(sel2[0]), sel2[1]),bit),\
+           AND(AND(sel2[0], NOT(sel2[1])),bit),\
+           AND(AND(sel2[0], sel2[1]),bit)
---
+++ gates_tests.py
@@ -0,0 +1,104 @@
+# Testing File with unittest
+
+import unittest
+import gates 
+
+class MyTest(unittest.TestCase):
+    # Test 1
+    def test_NAND(self):
+        nand_result = [1, 1, 1, 0]
+        for i in range(len(x)):
+            self.assertEqual(gates.NAND(x[i],y[i]), nand_result[i])
+
+    # Test 2
+    def test_NOT(self):
+        notx_result = [1, 1, 0, 0]
+        for i in range(len(x)):
+            self.assertEqual(gates.NOT(x[i]), notx_result[i])
+
+    # Test 3
+    def test_AND(self):
+        and_result = [0, 0, 0, 1]
+        for i in range(len(x)):
+            self.assertEqual(gates.AND(x[i],y[i]), and_result[i])
+
+    # Test 4
+    def test_OR(self):
+        or_result = [0, 1, 1, 1]
+        for i in range(len(x)):
+            self.assertEqual(gates.OR(x[i],y[i]), or_result[i])
+
+    # Test 5
+    def test_XOR(self):
+        xor_result = [0, 1, 1, 0]
+        for i in range(len(x)):
+            self.assertEqual(gates.XOR(x[i],y[i]), xor_result[i])
+
+    # Test 6
+    def test_MUX(self):
+        mux_result = [x[0], y[0]]
+        for i in range(len(sel)):
+            self.assertEqual(gates.MUX(x[0],y[0],sel[i]), mux_result[i])
+
+    # Test 7
+    def test_DMUX(self):
+        dmux_result = [(x[0], 0), (0, x[0])]
+        for i in range(len(sel)):
+            self.assertEqual(gates.DMUX(x[0],sel[i]), dmux_result[i])
+
+    # Test 8
+    def test_NOT16(self):
+        notx16_result = [1, 1, 0, 0] * 4
+        self.assertEqual(gates.NOT16(x16), notx16_result)
+
+    # Test 9
+    def test_AND16(self):
+        and16_result = [0, 0, 0, 1] * 4
+        self.assertEqual(gates.AND16(x16,y16), and16_result)
+
+    # Test 10
+    def test_OR16(self):
+        or16_result = [0, 1, 1, 1] * 4
+        self.assertEqual(gates.OR16(x16,y16), or16_result)
+
+    # Test 11
+    def test_MUX16(self):
+        mux16_result = [x16, y16]
+        for i in range(len(sel)):
+            self.assertEqual(gates.MUX16(x16,y16,sel[i]), mux16_result[i])
+
+    # Test 12
+    def test_OR_8Ways(self):
+        or_8ways_result = [1,0]
+        for i in range(len(x8_2)):
+            self.assertEqual(gates.OR_8Ways(x8_2[i]), or_8ways_result[i])
+
+    # Test 13
+    def test_MUX16_4Ways(self):
+        mux16_4ways_result = [x16, y16, z16, w16]
+        for i in range(len(sel2)):
+            self.assertEqual(gates.MUX16_4Ways(x16, y16, z16, w16, sel2[i]), mux16_4ways_result[i])
+
+    # Test 14
+    def test_DEMUX_4Ways(self):
+        demux_4ways_result = ((1, 0, 0, 0),
+                              (0, 1, 0, 0),
+                              (0, 0, 1, 0),
+                              (0, 0, 0, 1))
+        for i in range(len(sel2)):
+            self.assertEqual(gates.DEMUX_4Ways(1, sel2[i]), demux_4ways_result[i])
+
+if __name__ == "__main__":
+    #Inputs
+    x = [0, 0, 1, 1]
+    y = [0, 1, 0, 1]
+    sel = [0, 1]
+    x16 = [0, 0, 1, 1] * 4
+    y16 = [0, 1, 0, 1] * 4
+    x8_2 = [[0, 0, 0, 1] * 2, [0] * 8]
+    z16 = [0, 0, 0, 0] * 4
+    w16 = [1, 1, 1, 1] * 4
+    sel2 =[[0, 0], [0, 1], [1, 0], [1, 1]]
+
+    # Run the tests
+    unittest.main()
