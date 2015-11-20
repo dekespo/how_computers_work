056fe9dc2e60995adcf5f6d4ea21fe589aa6c639
dekespo <dekespo@gmail.com>
2015-11-20 12:29:00 +0100

    Redefining ALU, needs to handle 1 or -1 in 16-bit output

--- units/units.py
+++ units/units.py
@@ -5,15 +5,15 @@ import random, sys
 sys.path.insert(0, "../gates") # import the necessary directory
 import gates as G 
 
-# Part 1: The combination of gates to zero a value
+# Part 1: The combination of gates to set the value to 0
 def Zero16(x):
     return G.AND16(x, G.NOT16(x))
 
-# Part 2: The combination of gates to one a value
+# Part 2: The combination of gates to set the value to 1
 def One16(x):
     return G.OR16(x, G.NOT16(x))
 
-# Part 3: Convert one bit to nbits with wires
+# Part 3: Convert one bit to n bits with wires
 def wiresN(x, n):
     G.checkBit(x)
     return [x] * n
@@ -21,11 +21,39 @@ def wiresN(x, n):
 # Arithmetic Logic Unit (ALU) in 16 bits: ALU16
 def ALU16(x, y, zx, nx, zy, ny, f, no):
     n = 16
-    result = [random.randint(0,1) for i in range(n)] # Random noise in bits
-    print("result = ", result)
-    G.sizeCheckn(result, result, n)
-    # Line 1:
-    lineCheck1 = G.AND(zx, G.AND(G.NOT(nx), G.AND(zy, G.AND(G.NOT(zy), G.AND(f, G.NOT(no))))))
-    result = G.AND16(wiresN(lineCheck1, n), Zero16(x)) # Not sure if this one is a good way
-    return result
+    output = [random.randint(0,1) for i in range(n)] # Random noise in bits
+    print("current output = ", output)
+    G.sizeCheckn(output, output, n)
+
+    # Condition 1
+    if zx == 1: x = Zero16(x)
+    else: pass # x does not change
+
+    # Condition 2
+    if nx == 1: x = G.NOT16(x)
+    else: pass
+
+    # Condition 3
+    if zy == 1: y = Zero16(y)
+    else: pass # y does not change
+
+    # Condition 4
+    if ny == 1: y = G.NOT16(y)
+    else: pass
+
+    # Condition 5
+    if f == 1: output = G.Add16(x, y) 
+    else: output = G.AND16(x, y)
+
+    # Condition 6
+    if no == 1: output = G.NOT16(output)
+    else: pass
+
+    # Condition 7 & 8 for output check
+
+    # Line 1 to set the output to zero:
+    # Check if the parameter bits match
+    #lineCheck1 = G.AND(zx, G.AND(G.NOT(nx), G.AND(zy, G.AND(G.NOT(zy), G.AND(f, G.NOT(no))))))
+    #output = G.AND16(wiresN(lineCheck1, n), Zero16(x)) # Not sure if this one is a good way
+    return output
     
--- units/units_tests.py
+++ units/units_tests.py
@@ -2,11 +2,22 @@
 
 import unittest
 import units
+import gates as G 
 
 class MyTest(unittest.TestCase):
     # Test 1
     def test_ALU16(self):
         self.assertEqual(units.ALU16(x16, y16, 1, 0, 1, 0, 1, 0), zeros16)
+        #self.assertEqual(units.ALU16(x16, y16, 1, 1, 1, 1, 1, 1), ones16)
+        # TODO: Gives error how to define 1 or -1 in 16-bits?
+        self.assertEqual(units.ALU16(x16, y16, 0, 0, 1, 1, 0, 0), x16)
+        self.assertEqual(units.ALU16(x16, y16, 1, 1, 0, 0, 0, 0), y16)
+        self.assertEqual(units.ALU16(x16, y16, 0, 0, 1, 1, 0, 1), G.NOT16(x16))
+        self.assertEqual(units.ALU16(x16, y16, 1, 1, 0, 0, 0, 1), G.NOT16(y16))
+        self.assertEqual(units.ALU16(x16, y16, 0, 0, 0, 0, 1, 0), G.Add16(x16, y16))
+        self.assertEqual(units.ALU16(x16, y16, 0, 0, 0, 0, 0, 0), G.AND16(x16, y16))
+        self.assertEqual(units.ALU16(x16, y16, 0, 1, 0, 1, 0, 1), G.OR16(x16, y16))
+
 
 if __name__ == "__main__":
     x16 = [1, 0, 1, 0] * 4
