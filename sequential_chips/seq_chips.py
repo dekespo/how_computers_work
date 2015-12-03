f753f06f526da97c6e01868fd8c301a528619693
dekespo <dekespo@gmail.com>
2015-12-03 16:06:46 +0100

    RAMr added; a little change in Register

--- sequential_chips/seq_chips.py
+++ sequential_chips/seq_chips.py
@@ -1,5 +1,6 @@
 # List of units
-import random, sys
+import random, sys, math
+import numpy as np
 
 # They need logic gates
 sys.path.insert(0, "../gates") # import the necessary directory
@@ -8,43 +9,65 @@ import gates as G
 # Data-Flip-Folps (DFFs):
 # Warning: this one is assumed a primitive gate like NAND
 class DFF:
-    def __init__(self, a):
-        G.checkBit(a)
-        self.temp = random.randint(0,1) # Noise
-    def clock(self, nex):
-        G.checkBit(nex)
-        self.prev = self.temp
-        self.temp = nex
-        return self.prev
+	def __init__(self, a):
+		G.checkBit(a)
+		self.temp = random.randint(0,1) # Noise
+	def clock(self, nex):
+		G.checkBit(nex)
+		self.prev = self.temp
+		self.temp = nex
+		return self.prev
 
 # A single-bit register: BIT
 class BIT:
-    def __init__(self, a):
-        G.checkBit(a)
-        self.aDFF = DFF(a)
-        self.prev = random.randint(0,1) # Noise
-        self.prevLoad = random.randint(0,1) # Noise
-    def clock(self, nex, load):
-        G.checkBit(nex); G.checkBit(load)
-        part1 = G.AND(self.prevLoad, self.aDFF.clock(nex))
-        part2 = G.AND(G.NOT(self.prevLoad), self.prev)
-        self.prevLoad = load
-        self.prev = G.OR(part1, part2)
-        return self.prev
+	def __init__(self, a):
+		G.checkBit(a)
+		self.aDFF = DFF(a)
+		self.prev = random.randint(0,1) # Noise
+		self.prevLoad = random.randint(0,1) # Noise
+	def clock(self, nex, load):
+		G.checkBit(nex); G.checkBit(load)
+		part1 = G.AND(self.prevLoad, self.aDFF.clock(nex))
+		part2 = G.AND(G.NOT(self.prevLoad), self.prev)
+		self.prevLoad = load
+		self.prev = G.OR(part1, part2)
+		return self.prev
 
 #Register-16bit: REGISTER
 class REGISTER:
-    def __init__(self, a):
-        self.n = 16
-        G.sizeCheckn(a, a, self.n)
-        self.DFFs = []
-        for i in range(self.n):
-            self.DFFs.append(BIT(a[i]))
-        #self.prev = random.randint(0,1) # Noise
-        #self.prevLoad = random.randint(0,1) # Noise
-    def clock(self, nex, load):
-        G.sizeCheckn(nex, nex, self.n); G.checkBit(load)
-        output = []
-        for i in range(self.n):
-            output.append(self.DFFs[i].clock(nex[i], load))
-        return output
+	def __init__(self, a):
+		self.n = 16
+		G.sizeCheckn(a, a, self.n)
+		self.BITs = []
+		for i in range(self.n):
+			self.BITs.append(BIT(a[i]))
+	def clock(self, nex, load):
+		G.sizeCheckn(nex, nex, self.n); G.checkBit(load)
+		output = []
+		for i in range(self.n):
+			output.append(self.BITs[i].clock(nex[i], load))
+		return output
+
+# RAMr (where r is the number of registers) with 16 bits
+class RAMr:
+	def __init__(self, a, address, r):
+		self.k = math.log(r,2)
+		if len(address) != self.k:
+			sys.exit("THE NUMBER OF BITS IN ADDRESS DOES NOT MATCH WITH THE NUMBER OF REGISTERS. ABORTING THE MISSION!")
+		self.n = 16
+		G.sizeCheckn(a, a, self.n)
+		self.registers = {}
+		for i in range(r):
+			den = np.binary_repr(i, int(self.k))
+			self.registers[den] = REGISTER([random.randint(0, 1) for i in range(self.n)])
+		self.registers[address] = REGISTER(a)
+	# load = 0: read; load = 1: write, address should be string of bits
+	def clock(self, nex, load, address): 
+		G.sizeCheckn(nex, nex, self.n); G.checkBit(load)
+		for next in address:
+			G.checkBit(int(next))
+		if len(address) > self.k:
+			sys.exit("THE NUMBER OF BITS IN ADDRESS IS NOT LESS THAN THE NUMBER OF REGISTERS. ABORTING THE MISSION!")
+		output = self.registers[address].clock(nex, load)
+		return output
+
--- sequential_chips/seq_chips_tests.py
+++ sequential_chips/seq_chips_tests.py
@@ -5,61 +5,80 @@ import seq_chips
 import gates as G 
 
 class MyTest(unittest.TestCase):
-    # Test 1
-    def test_DFF(self):
-        arrx16, arry16 = [], []
-        xDFF, yDFF = seq_chips.DFF(x16[0]), seq_chips.DFF(y16[0])
-        for i in range(len(x16)):
-            arrx16.append(xDFF.clock(x16[i]))
-            arry16.append(yDFF.clock(y16[i]))
-        self.assertEqual(arrx16[1:], x16[:15]) 
-        self.assertEqual(arry16[1:], y16[:15]) 
+	# Test 1
+	def test_DFF(self):
+		arrx16, arry16 = [], []
+		xDFF, yDFF = seq_chips.DFF(x16[0]), seq_chips.DFF(y16[0])
+		for i in range(len(x16)):
+			arrx16.append(xDFF.clock(x16[i]))
+			arry16.append(yDFF.clock(y16[i]))
+		self.assertEqual(arrx16[1:], x16[:15]) 
+		self.assertEqual(arry16[1:], y16[:15]) 
 
-    # Test 2
-    def test_BIT(self):
-        arrx16Ones, arry16Ones = [], []
-        xBITOnes, yBITOnes = seq_chips.BIT(x16[0]), seq_chips.BIT(y16[0])
-        for i in range(len(loadOnes)):
-            arrx16Ones.append(xBITOnes.clock(x16[i], loadOnes[i]))
-            arry16Ones.append(yBITOnes.clock(y16[i], loadOnes[i]))
-        self.assertEqual(arrx16Ones[1:], x16[:15]) 
-        self.assertEqual(arry16Ones[1:], y16[:15]) 
+	# Test 2
+	def test_BIT(self):
+		arrx16Ones, arry16Ones = [], []
+		xBITOnes, yBITOnes = seq_chips.BIT(x16[0]), seq_chips.BIT(y16[0])
+		for i in range(len(loadOnes)):
+			arrx16Ones.append(xBITOnes.clock(x16[i], loadOnes[i]))
+			arry16Ones.append(yBITOnes.clock(y16[i], loadOnes[i]))
+		self.assertEqual(arrx16Ones[1:], x16[:15]) 
+		self.assertEqual(arry16Ones[1:], y16[:15]) 
 
-        arrx16Zeros, arry16Zeros = [], []
-        xBITZeros, yBITZeros = seq_chips.BIT(x16[0]), seq_chips.BIT(y16[0])
-        for i in range(len(loadZeros)):
-            arrx16Zeros.append(xBITOnes.clock(x16[i], loadZeros[i]))
-            arry16Zeros.append(yBITOnes.clock(y16[i], loadZeros[i]))
-        self.assertEqual(arrx16Zeros[1:], arrx16Zeros[:15]) 
-        self.assertEqual(arry16Zeros[1:], arry16Zeros[:15]) 
+		arrx16Zeros, arry16Zeros = [], []
+		xBITZeros, yBITZeros = seq_chips.BIT(x16[0]), seq_chips.BIT(y16[0])
+		for i in range(len(loadZeros)):
+			arrx16Zeros.append(xBITOnes.clock(x16[i], loadZeros[i]))
+			arry16Zeros.append(yBITOnes.clock(y16[i], loadZeros[i]))
+		self.assertEqual(arrx16Zeros[1:], arrx16Zeros[:15]) 
+		self.assertEqual(arry16Zeros[1:], arry16Zeros[:15]) 
 
-        arrxSpecial, arrySpecial = [], []
-        xBITSpecial, yBITSpecial = seq_chips.BIT(x16[0]), seq_chips.BIT(y16[0])
-        for i in range(len(loadSpecial)):
-            arrxSpecial.append(xBITOnes.clock(x16[i], loadSpecial[i]))
-            arrySpecial.append(yBITOnes.clock(y16[i], loadSpecial[i]))
-        self.assertEqual(arrxSpecial[1:], resultSpecialX) 
-        self.assertEqual(arrySpecial[1:], resultSpecialY) 
+		arrxSpecial, arrySpecial = [], []
+		xBITSpecial, yBITSpecial = seq_chips.BIT(x16[0]), seq_chips.BIT(y16[0])
+		for i in range(len(loadSpecial)):
+			arrxSpecial.append(xBITOnes.clock(x16[i], loadSpecial[i]))
+			arrySpecial.append(yBITOnes.clock(y16[i], loadSpecial[i]))
+		self.assertEqual(arrxSpecial[1:], resultSpecialX) 
+		self.assertEqual(arrySpecial[1:], resultSpecialY) 
 
-    # Test 3
-    def test_REGISTER(self):
-        given16tarr = [x16, y16, x16]
-        arr16t = []
-        arr16tRegister = seq_chips.REGISTER(x16)
-        for i in range(len(loadttimes)):
-            arr16t.append(arr16tRegister.clock(given16tarr[i], loadttimes[i]))
-        self.assertEqual(arr16t[1], x16) 
-        self.assertEqual(arr16t[2], y16) 
+	# Test 3
+	def test_REGISTER(self):
+		given16tarr = [x16, y16, x16]
+		arr16t = []
+		arr16tRegister = seq_chips.REGISTER(x16)
+		for i in range(len(loadttimes)):
+			arr16t.append(arr16tRegister.clock(given16tarr[i], loadttimes[i]))
+		self.assertEqual(arr16t[1], x16) 
+		self.assertEqual(arr16t[2], y16) 
+
+	#Test 4 # TODO: add the right testing methods
+	def test_RAMr(self):
+		given16tarr = [x16, y16, x16]
+		arr16t = []
+		address = "001"; r = 8
+		arr16tRAMr = seq_chips.RAMr(x16, address, r)
+		arr16t.append(arr16tRAMr.clock(given16tarr[0], loadttimes[0], address))
+		arr16t.append(arr16tRAMr.clock(given16tarr[1], loadttimes[1], address))
+		arr16t.append(arr16tRAMr.clock(given16tarr[2], 0, address))
+		arr16t.append(arr16tRAMr.clock(given16tarr[0], 0, "010"))
+		arr16t.append(arr16tRAMr.clock(given16tarr[0], 0, "010"))
+		arr16t.append(arr16tRAMr.clock(given16tarr[2], 0, address))
+		for next in arr16t:
+			print(next)
+		#for i in range(len(loadttimes)):
+		#    arr16t.append(arr16tRegister.clock(given16tarr[i], loadttimes[i]))
+		#self.assertEqual(arr16t[1], x16) 
+		#self.assertEqual(arr16t[2], y16) 
 
 if __name__ == "__main__":
-    x16 = [1, 0, 1, 0] * 4
-    y16 = [0, 0, 1, 1] * 4
-    loadOnes = [1] * 16
-    loadZeros = [0] * 16
-    loadSpecial = [1, 1, 0, 1, 1]
-    resultSpecialX = [1, 0, 0, 0]
-    resultSpecialY = [0, 0, 0, 1]
-    loadttimes = [1, 1, 1]
+	x16 = [1, 0, 1, 0] * 4
+	y16 = [0, 0, 1, 1] * 4
+	loadOnes = [1] * 16
+	loadZeros = [0] * 16
+	loadSpecial = [1, 1, 0, 1, 1]
+	resultSpecialX = [1, 0, 0, 0]
+	resultSpecialY = [0, 0, 0, 1]
+	loadttimes = [1, 1, 1]
 
-    #Run the tests
-    unittest.main()
+	#Run the tests
+	unittest.main()
